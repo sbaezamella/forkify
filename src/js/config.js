c9cf863337652ac316631e25c383a119e81e5cf1
sbaezamella <sbaezamella@gmail.com>
2019-06-19 01:15:35 -0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'c8af35b07f9db78b7855f2084cf8535b';
---
+++ src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { key, proxy } from '../config';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const result = await axios(
+                `${proxy}http://food2fork.com/api/search?key=${key}&q=${
+                    this.query
+                }`
+            );
+            this.result = result.data.recipes;
+            // console.log("TCL: Search -> getResults -> this.result", this.result)
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
